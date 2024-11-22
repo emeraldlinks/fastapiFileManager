18206250cd8ca79cca883a1a258118bec298f820
emeraldlinks <joechristophersc@com>
2024-11-23 00:08:08 +0100

    Changed the project name and updated the version after publishing on pypi

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="file-manager",
-    version="1.1.0",
+    name="fastapiFileManager",
+    version="1.1.1",
     description="A plug-and-play file management package for FastAPI.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
